42be9cfcd5b774013f2519ef60b22172e6f19942
GUZHAOY <zhaoyu.22@intl.zju.edu.cn>
2023-04-13 10:16:43 +0800

    cds_counter.py

--- Practical9/cds_counter.py
+++ Practical9/cds_counter.py
@@ -1,7 +1,8 @@
 import re
+#Defining DNA sequence
 seq='ATGCAATCGACTACGATCTGAGAGGGCCTAA'
-if re.match('ATG',seq):
-    list_number=re.findall(r'TAA|TAG|TGA',seq)
-    print(len(list_number))
+if re.match('ATG',seq): #Find the start codon
+    list_number=re.findall(r'TAA|TAG|TGA',seq) #Find the stop codon
+    print("The number of possible encoding sequences is",len(list_number)) #Output the number of encoding sequences found
 else:
     print(0)
