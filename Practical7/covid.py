46e569d289b61cfc4f9ec8ae1e19b1b01bd61ee2
GUZHAOY <zhaoyu.22@intl.zju.edu.cn>
2023-05-21 19:50:53 +0800

    Practical7

--- Practical7/covid.py
+++ Practical7/covid.py
@@ -1,76 +1,73 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Select the rows where the date is '2020-03-31' and the columns 'location', 'new_cases' and 'new_deaths'
-new_data = covid_data.loc[covid_data['date'] == '2020-03-31', ['location', 'new_cases', 'new_deaths']]
-
-# Compute the mean new cases and new deaths on 31 March using numpy
-mean_new_cases = np.mean(new_data['new_cases'])
-mean_new_deaths = np.mean(new_data['new_deaths'])
-
-# Print the mean new cases and new deaths
-print("Mean new cases on 31 March:", mean_new_cases)
-print("Mean new deaths on 31 March:", mean_new_deaths)
-
-# Compute the proportion of new deaths as a proportion of new cases on 31 March
-proportion_new_deaths = new_data['new_deaths'].sum() / new_data['new_cases'].sum()
-print("Proportion of new deaths as a proportion of new cases on 31 March:", proportion_new_deaths)
-
-# Plot boxplots of new cases and new deaths on 31 March using matplotlib
-fig, ax = plt.subplots()
-new_data.boxplot(column=['new_cases', 'new_deaths'], ax=ax)
-plt.title("New cases and new deaths on 31 March")
-plt.ylabel("Number of cases/deaths")
-plt.show()
-
-# Select the rows where the location is 'World' and the columns 'date' and 'new_cases'
-world_data = covid_data.loc[covid_data['location'] == 'World', ['date', 'new_cases']]
-
-# Convert the 'date' column to a datetime object and set it as the index
-world_data['date'] = pd.to_datetime(world_data['date'])
-world_data.set_index('date', inplace=True)
-
-# Resample the data to a weekly frequency and compute the cumulative sum
-world_data = world_data.resample('W').sum().cumsum()
-
-# Plot the new cases for the world over time using matplotlib
-world_dates = world_data.index
-world_new_cases = world_data['new_cases']
-plt.plot(world_dates, world_new_cases, 'r+')
-plt.title("New cases for the world over time")
-plt.xlabel("Date")
-plt.ylabel("Number of cases")
-plt.show()
-
-# Plot new cases and new deaths for the world over time using matplotlib
-world_data = covid_data.loc[covid_data['location'] == 'World', ['date', 'new_cases', 'new_deaths']]
-world_data['date'] = pd.to_datetime(world_data['date'])
-world_data.set_index('date', inplace=True)
-world_data = world_data.resample('W').sum().cumsum()
-
-fig, ax = plt.subplots()
-ax.plot(world_data.index, world_data['new_cases'], color='blue', label='New cases')
-ax.plot(world_data.index, world_data['new_deaths'], color='red', label='New deaths')
-plt.title("New cases and new deaths for the world over time")
-plt.xlabel("Date")
-plt.ylabel("Number of cases/deaths")
-plt.legend()
-plt.xticks(world_data.index[0:len(world_data.index):4], rotation=-90)
-plt.show()
-
-#Plot a boxplot of total case numbers in different countries on 31 March 2020
+import os
 import pandas as pd
-covid_data = pd.read_csv("full_data.csv")
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Select the rows where the date is '2020-03-31' and the column 'location' and 'total_cases'
-total_cases_data = covid_data.loc[covid_data['date'] == '2020-03-31', ['location', 'total_cases']]
+pd.set_option('display.max_columns', None)
+# show all columns
+os.chdir("D:/2023Grade1/IBI1")
+os.getcwd()
+os.listdir()
+covid_data = pd.read_csv("full_data.csv")
+# the second column from every 100th row from the first 1000 rows (inclusive)
+print(covid_data.iloc[0:1001:100, 1])
 
-# Create a boxplot of total case numbers in different countries on 31 March 2020 using matplotlib
-fig, ax = plt.subplots()
-total_cases_data.boxplot(column='total_cases', by='location', ax=ax)
-plt.xticks(rotation=-90)
-plt.title("Total case numbers in different countries on 31 March 2020")
-plt.ylabel("Number of cases")
+# "total cases" for all rows corresponding to Afghanistan.
+my_rows = []
+for i in range(len(covid_data)):
+    if covid_data.loc[i, "location"] == "Afghanistan":
+        my_rows.append(True)
+    else:
+        my_rows.append(False)
+print(covid_data.loc[my_rows, "total_cases"])
+
+# the mean number of new cases and new deaths on 31 March 2020.
+new_data1 = covid_data.loc[covid_data["location"] != "World", ["date", "location", "new_cases", "new_deaths"]]
+new_data = new_data1.loc[new_data1["date"] == "2020-03-31", ["location", "new_cases", "new_deaths"]]
+new_cases_mean = np.mean(new_data["new_cases"])
+new_deaths_mean = np.mean(new_data["new_deaths"])
+print(f"This mean has already exclude the rows called 'world'. The mean number for new cases "
+      f"on 2020-3-31 is {new_cases_mean},and the mean number for new deaths "
+      f"on 2020-3-31 is {new_deaths_mean}")
+
+# boxplot of new cases and new deaths on 31 March 2020. The rows "World" are excluded.
+plt.boxplot(x=new_data["new_deaths"])
+plt.title("new deaths on 2020-03-31")
+plt.show()
+plt.boxplot(x=new_data["new_cases"])
+plt.title("new cases on 2020-03-31")
 plt.show()
 
+data = pd.DataFrame({'new_cases': new_data["new_cases"], 'new_deaths': new_data["new_deaths"]})
+data.boxplot(column=['new_cases', 'new_deaths'])
+plt.title("new cases and deaths on 2020-03-31")
+plt.show()
+
+# plot both new cases and new deaths worldwide over time.
+world_data = covid_data.loc[covid_data["location"] == "World", "new_cases"]
+world_data1 = covid_data.loc[covid_data["location"] == "World", "new_deaths"]
+world_dates = covid_data.loc[covid_data["location"] == "World", "date"]
+
+plt.plot(figsize=(30, 5))
+plt.plot(world_dates, world_data, 'b--', label='new cases')
+plt.plot(world_dates, world_data1, 'r--', label='new deaths')
+plt.xticks(world_dates.iloc[0:len(world_dates):4])
+# show x ticks every 4 days.
+plt.xticks(rotation=90, fontsize=6)
+# rotate the ticks
+plt.ylabel('Number')
+plt.title("worldwide COVID-19 new deaths and new cases overtime")
+plt.legend()
+plt.show()
+
+# question
+china_data = covid_data.loc[covid_data['location'] == 'China', ['total_cases', 'total_deaths']]
+china_rate = china_data.apply(lambda china_data: china_data['total_deaths'] / china_data['total_cases'], axis=1)
+# let the value of total death / total cases write into each row.
+plt.plot(world_dates, china_rate, 'bo')
+plt.xticks(world_dates.iloc[0:len(world_dates):4])
+plt.xticks(rotation=90, fontsize=6)
+plt.title("China COVID-19 death rate overtime")
+plt.ylabel('Total death rate')
+
+plt.show()
